d8d017e3432662274b564bbc998a0b8fcb529ba6
Maik Wramba <mail@mkwa.dev>
2025-01-11 23:20:51 +0100

    Update miryoku_behaviors.h

--- miryoku/miryoku_behaviors.h
+++ miryoku/miryoku_behaviors.h
@@ -4,6 +4,7 @@
 #pragma once
 
 #define U_MT(MOD, TAP) &u_mt MOD TAP
-#define U_MT_L(MOD, TAP) &u_mt_l MOD TAP
-#define U_MT_R(MOD, TAP) &u_mt_r MOD TAP
+#define HML(MOD, TAP) &hml MOD TAP
+#define HMR(MOD, TAP) &hmr MOD TAP
 #define U_LT(LAYER, TAP) &u_lt LAYER TAP
+#define LR(LAYER, TAP) &lr LAYER TAP
