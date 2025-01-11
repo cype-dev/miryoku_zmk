84c2ceac383ae2cf8746576c90882b53acc02bb0
Maik Wramba <mail@mkwa.dev>
2025-01-11 23:23:36 +0100

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -3,6 +3,6 @@
 
 #define MIRYOKU_LAYER_BASE \
 &kp Q,             &kp W,             &kp F,             &kp P,             &kp B,             &kp J,             &kp L,             &kp U,             &kp Y,             &kp SQT,           \
-U_MT_L(LGUI, A),   U_MT_L(LALT, R),   U_MT_L(LCTRL, S),  U_MT_L(LSHFT, T),  &kp G,             &kp M,             U_MT_R(LSHFT, N),  U_MT_R(LCTRL, E),  U_MT_R(LALT, I),   U_MT_R(LGUI, O),     \
+HML(LGUI, A),      HML(LALT, R),      HML(LCTRL, S),     HML(LSHFT, T),     &kp G,             &kp M,             HMR(LSHFT, N),     HMR(LCTRL, E),     HMR(LALT, I),      HMR(LGUI, O),     \
 U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp D,             &kp V,             &kp K,             &kp H,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
-U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, SPACE),U_LT(U_MOUSE, TAB),U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
+U_NP,              U_NP,              U_LT(U_MEDIA, ESC),LR(U_NAV, SPACE),  U_LT(U_MOUSE, TAB),U_LT(U_SYM, RET),  LR(U_NUM, BSPC),   LR(U_FUN, DEL),    U_NP,              U_NP
