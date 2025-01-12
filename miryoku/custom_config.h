cd4eab313b187b8bc1c681baab8c0ab04bcbfd05
Maik Wramba <mail@mkwa.dev>
2025-01-12 17:52:13 +0100

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -27,21 +27,21 @@ MIRYOKU_X(INTL,   "Intl")
 #define U_INTL   10
 
 #define MIRYOKU_LAYER_BASE \
-&kp Q,             &kp W,             &kp F,             &kp P,             &kp B,             &kp J,             &kp L,             &kp U,             &kp Y,             &kp SQT,           \
-HML(LGUI, A),      HML(LALT, R),      HML(LCTRL, S),     HML(LSHFT, T),     &kp G,             &kp M,             HMR(LSHFT, N),     HMR(LCTRL, E),     HMR(LALT, I),      HMR(LGUI, O),     \
-U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp D,             &kp V,             &kp K,             &kp H,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
+&kp Q,             &kp W,             &kp F,             &kp P,             &kp B,             &kp J,             &kp L,             &kp U,             &kp Y,             &kp SQT,               \
+HML(LGUI, A),      HML(LALT, R),      HML(LCTRL, S),     HML(LSHFT, T),     &kp G,             &kp M,             HMR(LSHFT, N),     HMR(LCTRL, E),     HMR(LALT, I),      HMR(LGUI, O),          \
+U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp D,             &kp V,             &kp K,             &kp H,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH), \
 U_NP,              U_NP,              U_LT(U_MEDIA, ESC),LR(U_NAV, SPACE),  U_LT(U_MOUSE, TAB),U_LT(U_SYM, RET),  LR(U_NUM, BSPC),   LR(U_FUN, DEL),    U_NP,              U_NP
 
 #define MIRYOKU_LAYER_NAV \
-U_BOOT,            &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      U_NA,              U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
-&kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NA,              &u_caps_word,      &kp LEFT,          &kp DOWN,          &kp UP,            &kp RIGHT,         \
-U_NA,              &kp RALT,          &u_to_U_NUM,       &u_to_U_NAV,       U_NA,              &kp INS,           &kp HOME,          &kp PG_DN,         &kp PG_UP,         &kp END,           \
+ U_BOOT,            &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      U_NA,              U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,    \
+&kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NA,              &u_caps_word,      &kp LEFT,          &kp DOWN,          &kp UP,            &kp RIGHT, \
+U_NA,              &kp RALT,          &u_to_U_NUM,       &u_to_U_NAV,       U_NA,              &kp INS,           &kp HOME,          &kp PG_DN,         &kp PG_UP,         &kp END,   \
 U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              &kp RET,           U_LT(U_INTL, BSPC),&kp DEL,           U_NP,              U_NP
 
 #define MIRYOKU_LAYER_INTL \
-U_NA,              U_NA,              U_NA,              U_NA,              U_NA,              U_NA,              U_NA,              &kp ü,             U_NA,              U_NA,             \
-&kp ä,             U_NA,              &kp ß,             &kp LSHFT,         U_NA,              U_NA,              &kp LSHFT,         U_NA,              U_NA,              &kp ö,         \
-U_NA,              U_NA,              U_NA,              U_NA,              U_NA,              U_NA,              U_NA,              U_NA,              U_NA,              U_NA,           \
+U_NA,              U_NA,              U_NA,              U_NA,              U_NA,              U_NA,              U_NA,              &kp RA(y),         U_NA,              U_NA,      \
+&kp RA(q),         U_NA,              &kp RA(s),         &kp LSHFT,         U_NA,              U_NA,              &kp LSHFT,         U_NA,              U_NA,              &kp RA(p), \
+U_NA,              U_NA,              U_NA,              U_NA,              U_NA,              U_NA,              U_NA,              U_NA,              U_NA,              U_NA,      \
 U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              U_NA,              U_NA,              U_NA,              U_NP,              U_NP
 
 #define MIRYOKU_LAYERMAPPING_INTL MIRYOKU_MAPPING
