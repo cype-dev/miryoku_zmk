ac81d9c670049d28c19a3de7490f5e023faaf679
Maik Wramba <mail@mkwa.dev>
2025-01-13 16:36:57 +0100

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -32,12 +32,6 @@ U_HML(LGUI, A),    U_HML(LALT, R),    U_HML(LCTRL, S),    U_HML(LSHFT, T),    &k
 U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,              &kp D,              &kp V,              &kp K,             &kp H,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH), \
 U_NP,              U_NP,              U_LT(U_MEDIA, ESC), U_LR(U_NAV, SPACE), U_LT(U_MOUSE, TAB), U_LT(U_SYM, RET),  U_LR(U_NUM, BSPC), U_LR(U_FUN, DEL),  U_NP,              U_NP
 
-#define MIRYOKU_LAYER_NAV \
-U_BOOT,            &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      U_NA,              U_RDO,             U_PST,                U_CPY,             U_CUT,             U_UND,     \
-&kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NA,              &u_caps_word,      &kp LEFT,             &kp DOWN,          &kp UP,            &kp RIGHT, \
-U_NA,              &kp RALT,          &u_to_U_NUM,       &u_to_U_NAV,       U_NA,              &kp INS,           &kp HOME,             &kp PG_DN,         &kp PG_UP,         &kp END,   \
-U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              &kp RET,           U_LS(U_INTL, U_INTL), &kp DEL,           U_NP,              U_NP
-
 #define MIRYOKU_LAYER_MEDIA \
 U_BOOT,            &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      U_NA,              U_NA,              &kp C_RW,          U_NA,              U_NA,              &kp C_FF,          \
 &kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NA,              U_EP_TOG,          &kp C_PREV,        &kp C_VOL_DN,      &kp C_VOL_UP,      &kp C_NEXT,        \
